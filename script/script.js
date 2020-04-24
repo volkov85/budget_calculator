3f0b7df6c5925c30d0be32abb40ecccf86c190d8
Volkov85 <xplz2007@gmail.com>
2020-04-24 16:16:44 +0600

    Добавление операций

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
+const generateId = () => `calc${Math.round(Math.random * 1e8).toString(16)}`
+
 const totalBalance = document.querySelector('.total__balance'),
     totalMoneyIncome = document.querySelector('.total__money-income'),
     totalMoneyExpenses = document.querySelector('.total__money-expenses'),
     historyList = document.querySelector('.history__list'),
-    form = document.getElementById('#form'),
+    form = document.getElementById('form'),
     operationName = document.querySelector('.operation__name'),
     operationAmount = document.querySelector('.operation__amount');
 
@@ -58,14 +60,44 @@ const updateBalance = () => {
         .filter((item) => item.amount < 0)
         .reduce((result, item) => result + item.amount, 0);
 
-    totalMoneyIncome.textContent = resultIncome;
-    totalMoneyExpenses.textContent = resultExpenses;
+    totalMoneyIncome.textContent = resultIncome + ' ₽';
+    totalMoneyExpenses.textContent = resultExpenses + ' ₽';
+    totalBalance.textContent = (resultIncome + resultExpenses) + ' ₽';
+};
+
+const addOperation = (event) => {
+    event.preventDefault();
+
+    const operationNameValue = operationName.value,
+        operationAmountValue = operationAmount.value;
+
+    operationName.style.borderColor = '';
+    operationAmount.style.borderColor = '';
+
+    if (operationNameValue && operationAmountValue) {
+        const operation = {
+            id: generateId(),
+            description: operationNameValue,
+            amount: operationAmountValue,
+        };
+
+        dbOperation.push(operation);
+        init();
+    } else {
+        if (!operationNameValue) operationName.style.borderColor = 'red';
+        if (!operationAmountValue) operationAmount.style.borderColor = 'red';
+    }
+
+    operationName.value = '';
+    operationAmount.value = '';
 };
 
 const init = () => {
     historyList.textContent = '';
-    dbOperation.forEach(renderOperation);
+    dbOperation.forEach(renderOperation)
     updateBalance();
 };
+
+form.addEventListener('submit', addOperation);
 
 init();
